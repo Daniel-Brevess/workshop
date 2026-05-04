83db9b96135766b174582b9b428c60d205787b03
Daniel-Brevess <danielbreves1.20@gmail.com>
2026-05-04 17:28:33 -0300

    project created

---
+++ src/test/java/org/danielbreves/workshopapi/WorkshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.danielbreves.workshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
